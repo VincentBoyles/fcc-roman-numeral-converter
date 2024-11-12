974fb8ca98c3bc85422de45f1ec54a9a4ad2ae81
VincentBoyles <boylesvincentmanuel@gmail.com>
2024-11-12 22:35:42 +0800

    finish project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const input = document.getElementById("number");
-const message = document.getElementById("output")
-const romanNumerals = [
+const message = document.getElementById("output");
+const numbers = [
     { number: 1, roman: "I"},
     { number: 4, roman: "IV"},
     { number: 5, roman: "V"},
@@ -16,17 +16,92 @@ const romanNumerals = [
     { number: 1000, roman: "M"},
 ]
 
-function checkInput(input) {
-    invalidInput(input);
-}
-
-function invalidInput(input) {
-    if (input.value == "") {
-        alert("Please enter a valid number.")
-    } else if (input.value <= 0) {
-        alert("Please enter a number greater than 0.")
-    } else if (input.value > 3999) {
-        alert("Please enter a number lesser than 3999.")
+function checkInput() {
+    let value = input.value;
+    let error="";
+    if (value == "") {
+        error = "Please enter a valid number.";
+        message.innerHTML = `<span style="
+        display: flex;
+        margin: 20px;
+        border: solid var(--errorBorder);
+        background-color: var(--errorBox);
+        width: 350px;
+        height: 50px;
+        text-align: center;
+        font-size: 30px;
+        color: var(--errorColor);
+        font-weight: bold;
+        align-items: center;
+        justify-content: center;
+        padding: 30px;">
+        ${error}
+        </span>`;  
+    }else if (value <= 0) {
+       error = "Please enter a number greater than or equal to 1.";
+       message.innerHTML = `<span style="
+       display: flex;
+       margin: 20px;
+       border: solid var(--errorBorder);
+       background-color: var(--errorBox);
+       width: 350px;
+       height: 50px;
+       text-align: center;
+       font-size: 30px;
+       color: var(--errorColor);
+       font-weight: bold;
+       align-items: center;
+       justify-content: center;
+       padding: 30px;">
+       ${error}
+       </span>`;  
+    } else if (value > 3999) {
+        error = "Please enter a number less than or equal to 3999."
+        message.innerHTML = `<span style="
+        display: flex;
+        margin: 20px;
+        border: solid var(--errorBorder);
+        background-color: var(--errorBox);
+        width: 350px;
+        height: 50px;
+        text-align: center;
+        font-size: 30px;
+        color: var(--errorColor);
+        font-weight: bold;
+        align-items: center;
+        justify-content: center;
+        padding: 30px;">
+        ${error}
+        </span>`;        
+    } else {
+        let result = convertInput(value);
+        message.innerHTML = `<span style="
+        display: flex;
+        margin: 20px;
+        border: solid white;
+        background-color: var(--boxColor);
+        width: 350px;
+        height: 50px;
+        text-align: center;
+        font-size: 50px;
+        color: white;
+        font-weight: bold;
+        align-items: center;
+        justify-content: center;
+        padding: 30px;">
+        ${result}
+        </span>`;
     }
 }
 
+
+function convertInput (value) {
+    let result ="";
+        for (let i = numbers.length - 1; i >= 0; i--) {
+                while (value >= numbers[i].number) {
+                    result += numbers[i].roman;
+                    value -= numbers[i].number;
+            }
+        }
+        return result;
+    }
